cba593eb345958b2895db37009edeb3b4ba60a0c
Sasadhar Rana <sasadharrana825@gmail.com>
2026-06-07 22:20:50 +0530

    Update some middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,6 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://smart-cart-git-main-sasadhar-s-projects.vercel.app",
         "https://smart-cart-kappa-eight.vercel.app"
     ],
     allow_credentials=True,
