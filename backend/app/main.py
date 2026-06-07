0209f0e9f5e4fbd90d48e08bcc039b52866b12d8
Sasadhar Rana <sasadharrana825@gmail.com>
2026-06-07 22:05:09 +0530

    Integrate Middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://your-app.vercel.app"
+        "https://smart-cart-kappa-eight.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
